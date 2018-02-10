8f9f611a6c132f6d245b535c6971a6b6748049cb
ElPumpo <samo_kol@hotmail.com>
2018-02-10 19:40:39 +0100

    Added rounding

--- serverJailer.lua
+++ serverJailer.lua
@@ -8,7 +8,7 @@ TriggerEvent('es:addGroupCommand', 'jail', 'user', function(source, args, user)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if xPlayer.job.name == 'police' then
 		TriggerClientEvent("esx_jailer:jail", tonumber(args[1]), tonumber(args[2]))
-		TriggerClientEvent('chatMessage', source, 'DOMARE', { 0, 0, 0 }, GetPlayerName(tonumber(args[1])) ..' sitter nu i fängelse för '.. tonumber(args[2]) / 60 ..' minuter')
+		TriggerClientEvent('chatMessage', source, 'DOMARE', { 0, 0, 0 }, GetPlayerName(tonumber(args[1])) ..' sitter nu i fängelse för '.. round(tonumber(args[2]) / 60) ..' minuter')
 	else
 		TriggerClientEvent('chatMessage', source, "SYSTEM", { 255, 0, 0 }, "Insufficient Permissions.")
 	end
@@ -29,3 +29,7 @@ TriggerEvent('es:addGroupCommand', 'unjail', 'user', function(source, args, user
 end, function(source, args, user)
 	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficient Permissions.")
 end, {help = "Unjail people from jail", params = {{name = "id", help = "target id"}}})
+
+function round(x)
+  return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
+end
