da08feac529536741cf4360ae87bac5fd3c45dfe
ElPumpo <samo_kol@hotmail.com>
2018-02-10 21:08:31 +0100

    Cleanup code

--- serverJailer.lua
+++ serverJailer.lua
@@ -7,8 +7,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 TriggerEvent('es:addGroupCommand', 'jail', 'admin', function(source, args, user)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if xPlayer.job.name == 'police' then
-		TriggerClientEvent("esx_jailer:jail", tonumber(args[1]), tonumber(args[2]))
-		TriggerClientEvent('chatMessage', source, 'DOMARE', { 0, 0, 0 }, GetPlayerName(tonumber(args[1])) ..' sitter nu i fängelse för '.. round(tonumber(args[2]) / 60) ..' minuter')
+		TriggerEvent('esx_jailer:sendToJail', tonumber(args[1]), tonumber(args[2]))
 	else
 		TriggerClientEvent('chatMessage', source, "SYSTEM", { 255, 0, 0 }, "Insufficient Permissions.")
 	end
@@ -30,11 +29,12 @@ end, function(source, args, user)
 	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficient Permissions.")
 end, {help = "Unjail people from jail", params = {{name = "id", help = "target id"}}})
 
-function round(x)
-  return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
-end
-
 RegisterServerEvent('esx_jailer:sendToJail')
 AddEventHandler('esx_jailer:sendToJail', function(source, jailTime)
-  TriggerClientEvent('esx_jailer:jail', source, jailTime)
+	TriggerClientEvent('chatMessage', source, 'DOMARE', { 0, 0, 0 }, GetPlayerName(source) ..' sitter nu i fängelse för '.. round(jailTime / 60) ..' minuter')
+	TriggerClientEvent('esx_jailer:jail', source, jailTime)
 end)
+
+function round(x)
+	return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
+end
