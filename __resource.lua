5433a66ac1b3e671187a70e0ed9048c95212488f
ElPumpo <samo_kol@hotmail.com>
2018-05-21 18:07:21 +0200

    Time left is now drawed on screen, cleanup

--- __resource.lua
+++ __resource.lua
@@ -2,15 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Jailer'
 
-version '1.0.0'
-
-client_scripts {
-	'@es_extended/locale.lua',
-	'locates/en.lua',
-	'locates/sv.lua',
-	'config.lua',
-	'client/main.lua'
-}
+version '1.1.0'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
@@ -19,4 +11,12 @@ server_scripts {
 	'locates/sv.lua',
 	'config.lua',
 	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locates/en.lua',
+	'locates/sv.lua',
+	'config.lua',
+	'client/main.lua'
 }
--- locates/en.lua
+++ locates/en.lua
@@ -2,7 +2,7 @@ Locales ['en'] = {
 	['blip_name']          = 'prison',
 	['judge']              = 'JUDGE',
 	['escape_attempt']     = 'you are not allowed to escape the prison!',
-	['remaining_msg']      = 'there remains %s minutes until you are released from jail',
+	['remaining_msg']      = 'there remains ~b~%s~s~ seconds until you are released from jail',
 	['jailed_msg']         = '%s is now in jail for %s minutes',
 	['unjailed']           = '%s has been released from jail!'
 }
