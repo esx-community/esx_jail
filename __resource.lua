c757ab84549215cda508e76244d46555edb68673
renildomarcio <krsolucoesweb@gmail.com>
2018-05-30 00:15:47 -0300

    Update __resource.lua
    Update

--- __resource.lua
+++ __resource.lua
@@ -2,12 +2,13 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Jailer'
 
-version '1.1.0'
+version '1.1.1'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'locates/en.lua',
+	'locates/br.lua',
 	'locates/sv.lua',
 	'config.lua',
 	'server/main.lua'
@@ -16,6 +17,7 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'locates/en.lua',
+	'locates/br.lua',
 	'locates/sv.lua',
 	'config.lua',
 	'client/main.lua'
