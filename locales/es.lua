bb5517a31e2e3932bad7b327bd20be4dcca9c774
BackrndSource <back.rndsource@gmail.com>
2019-03-30 02:58:13 +0000

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,8 @@
+Locales ['es'] = {
+	['blip_name']          = 'prisión',
+	['judge']              = 'JUEZ',
+	['escape_attempt']     = 'no puedes escapar de la prisión...',
+	['remaining_msg']      = 'te quedan ~b~%s~s~ segundos hasta ser liberado',
+	['jailed_msg']         = '%s estará en la prisión %s minutos',
+	['unjailed']           = '%s ha salido de la prisión!'
+}
