b2dd3ae5d1baca530eb135ffb21ca7d41c61ee07
ElPumpo <samo_kol@hotmail.com>
2020-01-19 11:19:12 +0100

    Fixed locale indents

--- locales/en.lua
+++ locales/en.lua
@@ -1,8 +1,8 @@
 Locales ['en'] = {
-	['blip_name']          = 'bolingbroke Penitentiary',
-	['judge']              = 'JUDGE',
-	['escape_attempt']     = 'you are not allowed to escape the prison!',
-	['remaining_msg']      = 'there remains ~b~%s~s~ seconds until you are released from jail',
-	['jailed_msg']         = '%s is now in jail for %s minutes',
-	['unjailed']           = '%s has been released from jail!'
+  ['blip_name'] = 'bolingbroke Penitentiary',
+  ['judge'] = 'JUDGE',
+  ['escape_attempt'] = 'you are not allowed to escape the prison!',
+  ['remaining_msg'] = 'there remains ~b~%s~s~ seconds until you are released from jail',
+  ['jailed_msg'] = '%s is now in jail for %s minutes',
+  ['unjailed'] = '%s has been released from jail!'
 }
--- locales/sv.lua
+++ locales/sv.lua
@@ -1,8 +1,8 @@
 Locales ['sv'] = {
-	['blip_name']          = 'fängelse',
-	['judge']              = 'fängelse',
-	['escape_attempt']     = 'du får inte rymma från fängelset!',
-	['remaining_msg']      = 'det kvarstår ~b~%s~s~ sekunder tills du släpps från fängelset',
-	['jailed_msg']         = '%s sitter i fängelse i %s minuter!',
-	['unjailed']           = '%s har blivit befriad från fängelset!'
+  ['blip_name'] = 'fängelse',
+  ['judge'] = 'fängelse',
+  ['escape_attempt'] = 'du får inte rymma från fängelset!',
+  ['remaining_msg'] = 'det kvarstår ~b~%s~s~ sekunder tills du släpps från fängelset',
+  ['jailed_msg'] = '%s sitter i fängelse i %s minuter!',
+  ['unjailed'] = '%s har blivit befriad från fängelset!'
 }
